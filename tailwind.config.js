7e2a35a96e802c2e6c9e59e5a46c0e09cfb68796
Lydia Kwa <lydiakwa@gmail.com>
2023-04-20 15:28:12 -0400

    FEAT: created main QR Used tailwind in-line index.html. Also added custom configurations to tailwind like screen size and colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    screens: {
+      sm: '300px',
+    },
+    colors: {
+      blue: '#d5e2ef',
+      white: '#ffffff',
+      grey: '#9ea3ab',
+    },
     extend: {},
   },
   plugins: [],
